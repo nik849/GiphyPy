41ba9f405859f13c6a780b1ab7661a7e33a6de97
MichaelYusko <mykhaylo.yusko@gmail.com>
2017-07-08 02:09:14 +0300

    add random function

--- giphypy/client.py
+++ giphypy/client.py
@@ -28,7 +28,6 @@ class Giphy:
         self.session = session or aiohttp.ClientSession(loop=self.loop)
         self.params = {
             'api_key': self.api_key,
-            'q': None
         }
 
     async def _get(self, api_endpoint: str, **kwargs):
@@ -36,16 +35,15 @@ class Giphy:
         Wrapper for fetching data from Giphy
         :param api_endpoint: Giphy API endpoint, usually search or translate.
         """
-
         req_str = API_URL + api_endpoint
         async with self.session.get(url=req_str, params=self.params) as resp:
             data = await resp.json()
         return data
 
-    async def search(self, q, **kwargs):
+    async def search(self, query: str, **kwargs):
         """
         Main search method for Giphy's search endpoint
-        :param q: search term, Required
+        :param query: search term, Required
         :param limit: search result limit, not Required
         :param offset: search result offset
         :param rating: search result age rating (Y, G, PG, PG-13, R)
@@ -55,7 +53,7 @@ class Giphy:
         if kwargs:
             self.params.update(**kwargs)
 
-        self.params['q'] = q
+        self.params['q'] = query
         data = await self._get('search', params=self.params)
         if data['meta']['status'] is not 200:
             raise GiphyPyError(str(data['meta']['msg']))
@@ -77,14 +75,21 @@ class Giphy:
             raise GiphyPyError(str(data['meta']['msg']))
         return data
 
-    async def gif_links(self, q: str, **kwargs):
+    async def gif_links(self, query: str, **kwargs):
         """
-        :param q: Search by query.
+        :param query: Search by query.
         :param kwargs: limit/offset/rating/lang
         :return: an array with gif links
         """
-        data = await self.search(q, **kwargs)
+        data = await self.search(query, **kwargs)
         links = []
         for gif in data['data']:
             links.append(gif['url'])
         return links
+
+    async def random(self, **kwargs):
+        if kwargs:
+            self.params.update(**kwargs)
+
+        data = await self._get('random', params=self.params)
+        return data
--- giphypy/constants.py
+++ giphypy/constants.py
@@ -1,1 +1,2 @@
 API_URL = 'https://api.giphy.com/v1/gifs/'
+STICKERS_URL = 'https://api.giphy.com/v1/gifs/'
