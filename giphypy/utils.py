5db6c781637ef1aaeb9485e5c480e51f23a7a474
Nick Hale <nh7g15@soton.ac.uk>
2017-07-11 11:27:36 +0100

    Added 2 helpers

--- giphypy/utils.py
+++ giphypy/utils.py
@@ -4,14 +4,14 @@ def output_links(obj):
         links.append(gif['url'])
     return links
 
+
 def inline_gif(obj):
     return obj['data']['downsized']['url']
+
 
 def sort_by_max_size(obj, width, height):
     links = []
     for gif in obj['data']['images']:
         if (gif['width'] < width) and (gif['height'] < height):
-        links.append(gif['url'])
+            links.append(gif['url'])
     return links
-
-            
